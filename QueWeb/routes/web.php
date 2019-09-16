c593a39930c356574c8dec7538633e4d5b4d7884
yxb <yxb>
2019-09-16 19:36:13 +0800

    上传工作站

---
+++ QueWeb/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//登录页面
+Route::resource('login', 'LoginController');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
